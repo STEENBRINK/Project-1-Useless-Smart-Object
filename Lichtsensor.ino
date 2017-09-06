a4b474906d5e031b47cc2f09d25284b861f0b2f3
STEENBRINK <bram.steenbrink@hotmail.com>
2017-09-06 14:17:24 +0200

    Update Lichtsensor.ino
    Added the lcd display to it and made it work with 2 sensors

--- Lichtsensor.ino
+++ Lichtsensor.ino
@@ -1,63 +1,101 @@
-/* Photocell simple testing sketch. 
- 
-Connect one end of the photocell to 5V, the other end to Analog 0.
-Then connect one end of a 10K resistor from Analog 0 to ground 
-Connect LED from pin 11 through a resistor to ground 
-For more information see http://learn.adafruit.com/photocells */
- 
-int photocellPin = 0;     // the cell and 10K pulldown are connected to a0
-int photocellReading;     // the analog reading from the sensor divider
-int LEDpin = 11;          // connect Red LED to pin 11 (PWM pin)
-int LEDbrightness;        // 
-int readings[20];          // the analog reading from the sensor divider in an array
-int n = 0;
-int max_reading = 0;
-int min_reading = 10000;
+//include libraries
+#include <Wire.h>  
+#include <LiquidCrystal_I2C.h> // Using version 1.2.1
+
+//register lcd panel
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); 
+
+//register varables
+int photocellPin0 = 0;                // the cell and 10K pulldown are connected to a0
+int photocellPin1 = 1;                // the cell and 10K pulldown are connected to a1
+int photocellReading[2];              // the analog reading from the sensor dividers
+int readings[10][2];                  // the analog reading from the sensor0 dividers
+int n = 0;                            // number counter for remembering in array
+int max_reading[] = {0,0};            // maximum value read by sensors
+int min_reading[] = {10000, 10000};   // minumum value read by sensors
+boolean paper = false;                // used to switch the lcd
 
 void setup(void) {
-  // We'll send debugging information via the Serial monitor
-  Serial.begin(9600);   
+  // register lcd, sixteen characters across 2 lines
+  lcd.begin(16,2); 
+  lcd.backlight();
+  
+  // Only used for debug
+  //Serial.begin(9600);   
 }
  
 void loop(void) {
-  photocellReading = analogRead(photocellPin);  
+  //reads the value of the sensors
+  photocellReading[0] = analogRead(photocellPin0); 
+  //Serial.println("0 : " + String(photocellReading[0])); 
+  photocellReading[1] = analogRead(photocellPin1);  
+  //Serial.println("1 : " + String(photocellReading[1])); 
   
+  //remember the values read and process them
+  n++;
+  if(n == 10) { n=0; }
   Remember(photocellReading);
- 
-  // LED gets brighter the darker it is at the sensor
-  // that means we have to -invert- the reading from 0-1023 back to 1023-0
-  photocellReading = 1023 - photocellReading;
-  //now we have to map 0-1023 to 0-255 since thats the range analogWrite uses
-  LEDbrightness = map(photocellReading, 0, 1023, 0, 255);
-  analogWrite(LEDpin, LEDbrightness);
- 
+
+  CheckForSwitch();
   delay(50);
 }
 
-void Remember(int reading) {
-  //makes sure the array keeps looping
-  n++;
-  if(n == 20) { n=0; }
-  readings[n] = reading;
-
-  if( reading > max_reading){
-    max_reading = reading;
-    Serial.println(String(max_reading));
-    }
-  if( reading < min_reading){
-    min_reading = reading;
-    Serial.println(String(min_reading));
-    }
-
-  if (max_reading - min_reading >= 50){
-    Serial.println("SWITCH");
-    max_reading = 0;
-    min_reading = 10000;
-    for( int i = 0; i < 20; i++){
-      readings[i]=0;
+void Remember(int reading[]) {
+  //checks which sensor is read and writes the data to array
+  readings[n][0] = reading[0];
+  //Serial.println("0 : " + String(reading[0])); 
+  readings[n][1] = reading[1];
+  //Serial.println("1 : " + String(reading[1])); 
+  
+  //checks if any maximum or minumum values are read, if so write them into the array
+  for(int i = 0; i < 2; i++){
+    if( reading[i] > max_reading[i]){
+      //Serial.println("max: " + String(i) + " : " + String(max_reading[i]) + " old");
+      max_reading[i] = reading[i];
+      //Serial.println("max: " + String(i) + " : " + String(max_reading[i]));
       }
-    delay(2000);
+    if( reading[i] < min_reading[i]){
+      //Serial.println("min: " + String(i) + " : " + String(min_reading[i]) + " old");
+      min_reading[i] = reading[i];
+      //Serial.println("min: " + String(i) + " : " + String(min_reading[i]));
     }
-    
+  }
 }
+  
+void CheckForSwitch(){
+  //check if the values are extreme enough to switch
+  for(int i = 0; i < 2; i++){
+    if (max_reading[i] - min_reading[i] >= 50){
+      //initiates the switch
+      //Serial.println("SWITCH");
 
+      // switch text
+      //Serial.println("Clear lcd");
+      lcd.clear();
+      //checks the state of the display, then switches to the other
+      if (paper) {
+        lcd.setCursor(0,0);
+        lcd.print("| Wood Here!  |");
+        lcd.setCursor(0,1);
+        lcd.print("V Paper Here! V");
+        paper = !paper;
+      } else {
+        lcd.setCursor(0,0);
+        lcd.print("| Paper Here! |");
+        lcd.setCursor(0,1);
+        lcd.print("V Wood Here!  V");
+        paper = !paper;
+      }
+
+      //resets values to default
+      max_reading[0] = 0;
+      min_reading[0] = 10000;
+      max_reading[1] = 0;
+      min_reading[1] = 10000;
+      for( int j = 0; j < 10; j++){
+        readings[j][i]=0;
+      }
+    delay(500);
+    }
+  }
+}
